be9ac8cfd2b17ca57cac3e92db2ed19684b7c878
Miron Zabolotniev <photomiron@gmail.com>
2017-04-11 21:01:21 +0300

    frst

--- angular2-intro-master/app/app.component.ts
+++ angular2-intro-master/app/app.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
 
+const todos = [
+    'Изучить Джаваскрипт',
+    'Изучить Ангуляр 2',
+    'Написать приложение'
+];
+
 @Component({
+    moduleId: module.id,
     selector: 'app',
-    template: '<h1>Hello, World!</h1>'
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.css']
 })
 export class AppComponent {
+    title = 'Hello, World!';
+    todos = todos;
 
 }
